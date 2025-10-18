0e15455cd0cf7780343ca073430c06325cc5493f
Alan Soto <ing.alansoto@gmail.com>
2025-10-18 18:15:28 -0500

    first commit

---
+++ start/socket.ts
@@ -0,0 +1,13 @@
+import WebSocketServer from '../app/Services/web_socket_server.js'
+import server from '@adonisjs/core/services/server'
+
+WebSocketServer.boot(server.getNodeServer()!)
+
+WebSocketServer.io.on('connection', (socket) => {
+  console.log('Nuevo Dispositivo Conectado')
+  let id = socket.id
+  const body = socket.handshake.query
+  console.log('Body Del Socket ' + JSON.stringify(body))
+  console.log('Se Conectó ' + id)
+  socket.emit('notifications', { hello: 'world' })
+})
